8026b40fa7a1a9e531cd71e49d90b540a1185ee4
kadun1 <kadunjj@gmail.com>
2021-07-20 22:28:43 +0900

    Project ToYou 	BackEnd first Commit

---
+++ src/main/java/com/project/toyou_back/HelloController.java
@@ -0,0 +1,13 @@
+package com.project.toyou_back;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String HelloWorld(){
+        return "연동 테스트 World! \n";
+    }
+}
---
+++ src/test/java/com/project/toyou_back/ToyouBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.toyou_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyouBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
