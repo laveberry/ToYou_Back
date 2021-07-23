6b7d20b5516eef0221f6fd6ba1adcd1103951e92
kadun1 <kadunjj@gmail.com>
2021-07-24 00:38:05 +0900

    *투유
    	WebConfig 연동 확인

--- src/main/java/com/project/toyou_back/HelloController.java
+++ src/main/java/com/project/toyou_back/HelloController.java
@@ -1,5 +1,6 @@
 package com.project.toyou_back;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String HelloWorld(){
+
         return "연동 테스트 World! \n";
     }
 }
---
+++ src/main/java/com/project/toyou_back/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.project.toyou_back.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebConfig implements WebMvcConfigurer {
+
+    public WebConfig() {
+        System.out.println("생성자");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3000);
+    }
+}
